eeebbb84897f1d90833c9d35c265bfaf1121bf2a
agangotia <agangotia@gmail.com>
2014-07-28 00:18:21 -0500

    Message Added

---
+++ src/com/utd/dfs/msg/Message.java
@@ -0,0 +1,104 @@
+package com.utd.dfs.msg;
+
+import java.io.Serializable;
+
+/**
+ * Class Message
+ *  Messsage Types:
+ * 	Type 0 : Read Broadcast Request
+ *  Type 1 : Read Reply Yes
+ *  Type 2 : Read Reply No
+ *  
+ *  Type 10 :Write Broadcast Request
+ *  Type 11 :Write Reply Yes
+ *  Type 12 :Write Reply No
+ *  
+ * @author Anupam Gangotia
+ * Profile::http://en.gravatar.com/gangotia
+ * github::https://github.com/agangotia
+*/
+public class Message implements Serializable {
+
+
+
+	private static final long serialVersionUID = 1L;
+	private String messageId;
+	
+
+
+	private int senderNodeID;
+	private int recipientNodeID;
+	
+	private int msgType;
+	
+	private String data;
+	
+	private int logicalClockValue;
+	
+	public Message(){
+		
+	}
+	
+	public Message(String messageId, int senderNodeID, int recipientNodeID,
+			int msgType, String data, int logicalClockValue) {
+		super();
+		this.messageId = messageId;
+		this.senderNodeID = senderNodeID;
+		this.recipientNodeID = recipientNodeID;
+		this.msgType = msgType;
+		this.data = data;
+		this.logicalClockValue = logicalClockValue;
+	}
+
+	public String getMessageId() {
+		return messageId;
+	}
+
+	public void setMessageId(String messageId) {
+		this.messageId = messageId;
+	}
+
+	public int getSenderNodeID() {
+		return senderNodeID;
+	}
+
+	public void setSenderNodeID(int senderNodeID) {
+		this.senderNodeID = senderNodeID;
+	}
+
+	public int getRecipientNodeID() {
+		return recipientNodeID;
+	}
+
+	public void setRecipientNodeID(int recipientNodeID) {
+		this.recipientNodeID = recipientNodeID;
+	}
+
+	public int getMsgType() {
+		return msgType;
+	}
+
+	public void setMsgType(int msgType) {
+		this.msgType = msgType;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public int getLogicalClockValue() {
+		return logicalClockValue;
+	}
+
+	public void setLogicalClockValue(int logicalClockValue) {
+		this.logicalClockValue = logicalClockValue;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
