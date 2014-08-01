7c87a03b4d40e2efebe57b13a3e811e13834c1b1
agangotia <agangotia@gmail.com>
2014-07-31 23:23:18 -0500

    ..

--- cpysrc.sh
+++ cpysrc.sh
@@ -5,11 +5,20 @@ echo "Script started"
 DIRDST=$HOME"/AosProjectDemo/DemoLocal2Nodes/Project2"
 #This is the location from where i need to copy
 DIRSRC=$HOME"/Workspace_AOS/Project2.git"
-#number of processes to be setup
 
+#Clear the bin
 rm -r $DIRDST"/bin"
+#Clear the source
 rm -r $DIRDST"/src"
+#Clear the build.xml
 rm -r $DIRDST"build.xml"
+#Clear the log
+rm -r $DIRDST"/log/*"
+#Clear the filesystem
+rm -r $DIRDST"/fs/1/*"
+rm -r $DIRDST"/fs/2/*"
+#Clear the config_file
+rm -r $DIRDST"config_file"
 cp -r $DIRSRC"/src" $DIRDST
 cp -r $DIRSRC"/build.xml" $DIRDST
 echo "Copying successfull for "$DIRDST"/"process
