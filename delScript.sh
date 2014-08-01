99b2d673d79ef8f418984e77be3ad58e46c1c20d
agangotia <agangotia@gmail.com>
2014-08-01 00:06:06 -0500

    ..

--- delScript.sh
+++ delScript.sh
@@ -5,14 +5,12 @@
  
 echo "Script started"
 
-DIRDST=$HOME"/AosProjectDemo/DemoLocal2Nodes/Project2"
-
 #Clear the log
-rm -r $DIRDST"/log/*"
+rm -R log/*
 #Clear the filesystem
-rm -r $DIRDST"/fs/1/*"
-rm -r $DIRDST"/fs/2/*"
+rm -r fs/1/*
+rm -r /fs/2/*
 #Clear the config_file
-rm -r $DIRDST"config_file"
+rm -r config_file
 
 echo "Script completed"
